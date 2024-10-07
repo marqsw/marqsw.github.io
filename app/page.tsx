1619c591fef2a53d3362c924183a46fcd227295d
Marquis <marqswong@gmail.com>
2024-10-07 02:06:45 +0100

    feat: updated homepage with self introduction

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Avatar from "./components/Avatar";
 import LinkButton from "./components/LinkButton";
 import SectionTitle from "./components/Section/SectionTitle";
 import CardGrid from "./components/CardGrid";
+import SectionHeading from "./components/Section/SectionHeading";
+import Section from "./components/Section";
+import SectionParagraph from "./components/Section/SectionParagraph";
 
 // projects in reverse chronological order
 const projects = [
@@ -22,7 +25,7 @@ const projects = [
     description:
       "An elegant and responsive website to organises and introduces some projects of mine",
     imagePath: "/home/portfolio-website-cover.png",
-    link: "https://marqsw.github.io/",
+    // link: "https://marqsw.github.io/",
   },
 
   {
@@ -63,8 +66,17 @@ const contacts = [
 export default function Home() {
   return (
     <div className="grid grid-cols-1 justify-items-center gap-10">
-      <div className="w-52">
-        <Avatar imagePath="/home/profile-picture.jpeg"></Avatar>
+      <div className="flex flex-col items-center gap-10 md:flex-row md:gap-20">
+        <div className="w-64">
+          <Avatar imagePath="/home/profile-picture.jpeg"></Avatar>
+        </div>
+        <div className="flex w-72 flex-col gap-4 text-center md:text-left">
+          <div>
+            <SectionHeading>Hi, I am</SectionHeading>
+            <SectionTitle>Marquis</SectionTitle>
+          </div>
+          <p>I am a full stack developer and a mechaincal engineer student</p>
+        </div>
       </div>
 
       <div className="flex min-w-3.5 flex-col justify-center gap-5 sm:flex-row">
@@ -80,10 +92,22 @@ export default function Home() {
         })}
       </div>
 
-      {/* <SectionTitle>Who Am I?</SectionTitle> */}
+      <Section>
+        <SectionTitle>But who am I really?</SectionTitle>
+        <SectionParagraph>
+          I am a tech enthusiast who continuously challenge myself to solve
+          problems and create better work. This is why optimising code and
+          building user interface is my favourite part of the job. As a
+          mechanical engineer student at the same time, I also like exploring new things and
+          learn how they work. I never get tired of building things and excel in
+          problem-solving and lateral thinking.
+        </SectionParagraph>
+      </Section>
 
-      <SectionTitle>Recent Projects</SectionTitle>
-      <CardGrid cardProps={projects}></CardGrid>
+      <Section>
+        <SectionTitle>Recent Projects</SectionTitle>
+        <CardGrid cardProps={projects}></CardGrid>
+      </Section>
     </div>
   );
 }
