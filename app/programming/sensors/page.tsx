64fcce59689103587c2bb0fbbdf98802ef30bcdc
Marquis <marqswong@gmail.com>
2024-11-16 23:43:22 +0000

    fix: `"` can be escaped with `&quot;`, `&ldquo;`, `&#34;`, `&rdquo;`.  react/no-unescaped-entities

--- app/programming/sensors/page.tsx
+++ app/programming/sensors/page.tsx
@@ -25,52 +25,57 @@ export default function Page() {
       <Section>
         <Heading>About this Project</Heading>
         <Paragraph>
-          Smartphone is a piece of metal embedded with cutting-edge
+          {`          Smartphone is a piece of metal embedded with cutting-edge
           technologies. This includes a variety of sensors that could make it a
           handy measurement tool. However these sensors are not easily
           accessible in any user-friendly sense. This is why I have set out to
           create an app, that is easy on eye and user friendly, to instantly
           visualise data and record it for analysis.
+`}{" "}
         </Paragraph>
         <Heading>A simple and intuitive interface</Heading>
         <Section>
           <Heading>Neumorphism</Heading>
 
-          <ImageBanner imagePath="/programming/sensors/neumorphism.png"/>
+          <ImageBanner imagePath="/programming/sensors/neumorphism.png" />
           <Paragraph>
-            It's all about lights and shadows. This ingenious design style
+            {`            It's all about lights and shadows. This ingenious design style
             creates an illusion of extruded surfaces from the background,
             striking a balance between Skeuomorphism, which mimic real world
             textures, and minimalism. The result is a minimal, calm and quiet
             interface, eliminating any distractions and neutralising the busy
             live data.
+`}{" "}
           </Paragraph>
 
           <Heading>Sensor Cards</Heading>
-          <ImageBanner imagePath="/programming/sensors/sensor-cards.gif"/>
+          <ImageBanner imagePath="/programming/sensors/sensor-cards.gif" />
           <Paragraph>
-            The sensors and their data are grouped into cards, tapping on it
+            {`            The sensors and their data are grouped into cards, tapping on it
             would reveal their descriptions, allow viewing and selecting to be
             easily done.
+`}{" "}
           </Paragraph>
 
           <Heading>Control Panel</Heading>
-          <ImageBanner imagePath="/programming/sensors/control-panel.gif"/>
+          <ImageBanner imagePath="/programming/sensors/control-panel.gif" />
 
           <Paragraph>
-            This little compact panel houses most of the functions needed for
+            {`            This little compact panel houses most of the functions needed for
             configuring and recording the sensors. When is not used, it would
             shyly hide away and sit at the bottom. It also acts a "finish"
             button when selecting sensors for recording, expanding again to
             record the video, simplifying workflow and further simplifying the
             user interface.
+`}{" "}
           </Paragraph>
         </Section>
         <Heading>Installation</Heading>
         <Paragraph>
-          Pre-compiled APK downloads are available for Android. iOS users may
+          {`          Pre-compiled APK downloads are available for Android. iOS users may
           need to compile the app themselves as distributing the app as IPA
           format would require a paid subscription to Apple Developer Program.
+`}{" "}
         </Paragraph>
       </Section>
 
@@ -81,46 +86,39 @@ export default function Page() {
         </div>
       </LinkButton>
 
+      <Section>
+        <Heading>Building the app</Heading>
         <Section>
-          <Heading>Building the app</Heading>
-          <Section>
-            <Heading>Setting Up Environment</Heading>
-            <div className="flex flex-col sm:flex-row gap-5 justify-center ">
-              <LinkButton link="https://reactnative.dev/docs/set-up-your-environment">
-                <div className="flex items-center gap-5 text-center">
-                  <img
-                    className="h-7"
-                    src="/programming/sensors/react-logo.svg"
-                  />
-                  <p>Set Up React Native</p>
-                </div>
-              </LinkButton>
-              <LinkButton link="https://docs.expo.dev/get-started/set-up-your-environment/">
-                <div className="flex items-center gap-5 text-center">
-                  <img
-                    className="h-7"
-                    src="/programming/sensors/expo-logo.png"
-                  />
-                  <p>Set Up Expo</p>
-                </div>
-              </LinkButton>
-            </div>
-            <Heading>Building the App</Heading>
-            Clone the repository from Github
-            <CodeBlock>git clone https://github.com/marqsw/Sensors</CodeBlock>
-            <CodeBlock>cd Sensors</CodeBlock>
-            Install dependencies using npm
-            <CodeBlock>npm install</CodeBlock>
-            Build the app via EAS
-            <CodeBlock>
-              eas build --platform android --profile preview
-            </CodeBlock>
-            or
-            <CodeBlock>
-              eas build --platform ios --profile preview
-            </CodeBlock>
-          </Section>
+          <Heading>Setting Up Environment</Heading>
+          <div className="flex flex-col justify-center gap-5 sm:flex-row">
+            <LinkButton link="https://reactnative.dev/docs/set-up-your-environment">
+              <div className="flex items-center gap-5 text-center">
+                <img
+                  className="h-7"
+                  src="/programming/sensors/react-logo.svg"
+                />
+                <p>Set Up React Native</p>
+              </div>
+            </LinkButton>
+            <LinkButton link="https://docs.expo.dev/get-started/set-up-your-environment/">
+              <div className="flex items-center gap-5 text-center">
+                <img className="h-7" src="/programming/sensors/expo-logo.png" />
+                <p>Set Up Expo</p>
+              </div>
+            </LinkButton>
+          </div>
+          <Heading>Building the App</Heading>
+          Clone the repository from Github
+          <CodeBlock>git clone https://github.com/marqsw/Sensors</CodeBlock>
+          <CodeBlock>cd Sensors</CodeBlock>
+          Install dependencies using npm
+          <CodeBlock>npm install</CodeBlock>
+          Build the app via EAS
+          <CodeBlock>eas build --platform android --profile preview</CodeBlock>
+          or
+          <CodeBlock>eas build --platform ios --profile preview</CodeBlock>
         </Section>
+      </Section>
 
       <LinkButton link="https://github.com/marqsw/Sensors">
         <div className="flex gap-5">
